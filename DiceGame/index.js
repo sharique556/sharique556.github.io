c21296b39ec7e42735bd0008543d3bd62e135d91
talk2sohail <talk2sohail100@gmail.com>
2020-08-02 18:16:30 +0530

    added refresh button

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -1,37 +1,34 @@
-
 // For first Dice //
 
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice"+randomNumber1+".png";
-var randomImageSource = "images/"+randomDiceImage;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src",randomImageSource);
-
-
-
+image1.setAttribute("src", randomImageSource);
 
 // For first Dice //
 
-var randomNumber2 = Math.floor(Math.random()*6)+1 ;
-var randomDiceImage2 = "dice"+randomNumber2+".png";
-var randomImageSource2 = "images/"+randomDiceImage2;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
 
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src",randomImageSource2);
+image2.setAttribute("src", randomImageSource2);
 
 //Condition for decision //
 
-if(randomNumber1 > randomNumber2)
-{
-    document.querySelector("h1").innerHTML = "Refresh Me<br>Player 1 wins!"
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "Refresh Me<br>Player 1 wins!";
+} else if (randomNumber1 < randomNumber2) {
+	document.querySelector("h1").innerHTML = "Refresh Me<br>Player 2 wins!";
+} else {
+	document.querySelector("h1").innerHTML = " Refresh Me<br>oopps..Draw!";
 }
-else if(randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Refresh Me<br>Player 2 wins!"
-}
-else{
-    document.querySelector("h1").innerHTML = " Refresh Me<br>oopps..Draw!"
+
+function refresh() {
+	window.location.reload();
 }
