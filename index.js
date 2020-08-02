29607fe4d8e4df4ca42339496d2fed846296dcc6
Md Sharique <64912234+sharique556@users.noreply.github.com>
2020-08-02 13:18:25 +0530

    Added new files

---
+++ index.js
@@ -0,0 +1,37 @@
+
+// For first Dice //
+
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomImageSource = "images/"+randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+
+
+
+// For first Dice //
+
+var randomNumber2 = Math.floor(Math.random()*6)+1 ;
+var randomDiceImage2 = "dice"+randomNumber2+".png";
+var randomImageSource2 = "images/"+randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImageSource2);
+
+//Condition for decision //
+
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = " Player 1 wins!"
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = " Player 2 wins!"
+}
+else{
+    document.querySelector("h1").innerHTML = "opppsss...Draw!"
+}
